8a8ac052a8ace279a26ecc018d02f4cc61a78e22
Vern0n08 <vernontandoc@gmail.com>
2020-05-16 18:12:48 +0800

    Updates

---
+++ precache-manifest.9c782b9efa64a93db73f3bbd43b8a168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44039980c797ffa48f1fe00beb4ccee",
+    "url": "/Monster-rodlex/index.html"
+  },
+  {
+    "revision": "d2e44b874c22075f9dbe",
+    "url": "/Monster-rodlex/static/css/main.875fb08c.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Monster-rodlex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-rodlex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e44b874c22075f9dbe",
+    "url": "/Monster-rodlex/static/js/main.99a60a45.chunk.js"
+  },
+  {
+    "revision": "62cc2ddaf2b984a50570",
+    "url": "/Monster-rodlex/static/js/runtime-main.b12237a2.js"
+  }
+]);
